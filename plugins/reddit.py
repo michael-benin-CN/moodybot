59ddfd9032bc856ae04bb297eff189653daf0bd8
Joshua Holbrook <josh.holbrook@gmail.com>
2014-05-18 03:05:13 -0400

    Reddit plugin appears to work now

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -6,14 +6,15 @@ import grequests
 
 from pyaib.plugins import every, plugin_class
 
+
 @plugin_class('reddit')
 class RedditTailer(object):
     def __init__(self, ctx, config):
-        self.subreddits = config.reddit.subreddits
+        self.subreddits = config.subreddits
         self.latest = None
-        self.channels = config.reddit.channels or ctx.channels
+        self.channels = config.channels or ctx.channels.channels
 
-    @every(15 * 60, 'tail-reddit')
+    @every(5, 'tail-reddit')
     def tail_reddit(self, ctx, name):
         responses = grequests.map([
             grequests.get('http://www.reddit.com/r/{subreddit}/new.json'.format(subreddit=subreddit))
@@ -21,30 +22,36 @@ class RedditTailer(object):
         ])
 
         for res in responses:
-            data = res.json()
+            data = res.json()['data']
             posts = []
 
             if self.latest is None:
-                posts.append(data.children[0]['data'])
+                posts.append(data['children'][0]['data'])
             else:
                 is_new_enough = True
+                _i = 0
 
                 while is_new_enough:
-                    is_new_enough = int(child['data']['id']) > self.latest
+                    child = data['children'][_i]
+                    is_new_enough = float(child['data']['created']) > self.latest
                     if is_new_enough:
                         posts.append(child['data'])
+                        _i += 1
 
-                posts = posts.sort(sort_posts).reverse()
+            posts = sorted(posts, key=lambda p: p['created'])
+            posts.reverse()
 
             if len(posts) > 0:
-                self.latest = int(posts[0]['id'])
+                self.latest = float(posts[0]['created'])
 
             posts.reverse()
             for channel in self.channels:
                 for post in posts:
+                    score = post['score']
+                    title = post['title']
+                    u = post['author']
+                    r = post['subreddit']
+                    url = post['url']
                     ctx.PRIVMSG(channel, '⬆ ⬇ {score} {title} submitted by {u} to {r} {url}'.format(
-                        score=post.score,
-                        title=post.title,
-                        u=post.author,
-                        r=post.subreddit
+                        score=score, title=title, u=u, r=r, url=url
                     ))
